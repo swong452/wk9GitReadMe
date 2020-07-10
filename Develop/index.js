769d96e981103870c92aaa25ae74781f45e6ff6f
samson wong <steelerwong2005@hotmail.com>
2020-07-10 01:17:28 -0400

    README.md created with basic info from indexSamson.js

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var inquirer = require ("inquirer");
+var dotenv = require ("dotenv");
+
+// Store CLI arguments
+
+
+// Prompt questions
+
+
+
+const questions = [
+
+];
+
+
+// Create README.md file
+function writeToFile(fileName, data) {
+}
+
+
+// Main Program
+function init() {
+
+}
+
+init();
---
+++ Develop/indexSamson.js
@@ -0,0 +1,127 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+var dotenv = require("dotenv");
+const axios = require("axios");
+
+
+// https://www.makeareadme.com/
+
+// Prompt questions
+// const loginType = process.argv[2];
+
+function askQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is Git user name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is your Git Project Title ?",
+                name: "project"
+            },
+            {
+                type: "input",
+                message: "Project Description ?",
+                name: "projectDesc"
+            },
+            {
+                type: "input",
+                message: "GitHubemail ?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Installation ?",
+                name: "install"
+            }
+        ])
+        .then(function (response) {
+
+            //const gitProjectDetail = response.name;
+            let readMe = gitProjectInfo (response);
+
+            fs.writeFile("README.md", readMe, function(err) {
+                if (err) {
+                  throw err;
+                }
+        
+                console.log(`Saved repos`);
+            });
+        });
+} // end askQuestions
+
+function gitProjectInfo(gitInfo) {
+
+    return `
+    # Project: ${gitInfo.project} 
+    _Project Description_: ${gitInfo.projectDesc}.
+
+
+    **GitHub username is ${gitInfo.install} **
+    ## GitHubEmail: ${gitInfo.email}
+    > This is the END`;
+}
+
+
+
+
+
+// const questions = [
+
+// ];
+
+
+// Create README.md file
+// function writeToFile(fileName, data) {
+// }
+
+
+// Main Program
+function init() {
+    askQuestions();
+    //writeReadMe();
+
+}
+
+init();
+
+
+
+/*
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+
+
+      const queryUrl = `https://api.github.com/users/${response.username}/repos?per_page=100`;
+
+            axios.get(queryUrl).then(function (res) {
+                const repoNames = res.data.map(function (repo) {
+                    return repo.name;
+                });
+
+                const repoDesc = res.data.map(function(repo) {
+                    return repo.description
+                })
+
+                const repoLic = res.data.map(function(repo) {
+                    return repo.license
+                })
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+
+  */
